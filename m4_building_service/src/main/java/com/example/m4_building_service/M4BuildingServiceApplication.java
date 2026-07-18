36d7b84f3353267df41c063142d4dd6a926b1b84
itzzz_as <2023371052@uteq.edu.mx>
2026-07-18 12:51:03 -0600

    Conexiondebacke

--- m4_building_service/src/main/java/com/example/m4_building_service/M4BuildingServiceApplication.java
+++ m4_building_service/src/main/java/com/example/m4_building_service/M4BuildingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.m4_building_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class M4BuildingServiceApplication {
 
 	public static void main(String[] args) {
--- m5_room-service/src/main/java/com/example/m5_room_service/M5RoomServiceApplication.java
+++ m5_room-service/src/main/java/com/example/m5_room_service/M5RoomServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.m5_room_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class M5RoomServiceApplication {
 
 	public static void main(String[] args) {
